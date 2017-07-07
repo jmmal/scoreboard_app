17a8e1878cb56d1dbbe3ac91cd266421088852ad
Josh Maloney <josh@2001-44b8-022c-da00-7143-4fbb-f4b5-ff43.static.ipv6.internode.on.net>
2017-07-07 22:08:15 +0930

    Basic setup

---
+++ ScoreboardApp/Data.swift
@@ -0,0 +1,13 @@
+//
+//  Data.swift
+//  ScoreboardApp
+//
+//  Created by Josh Maloney on 7/7/17.
+//  Copyright © 2017 Josh Maloney. All rights reserved.
+//
+
+import Foundation
+
+class Games
+{
+}
--- ScoreboardApp/ViewController.swift
+++ ScoreboardApp/ViewController.swift
@@ -9,17 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK: Properties
+    
+    @IBOutlet weak var awayTeam: UILabel!
+    @IBOutlet weak var homeTeam: UILabel!
+    
+    private let gamesModel = Games()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        
     }
 
+
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    //MARK: Private Functions
+    private func listGames() {
+        awayTeam.text = "Red Sox"
+        homeTeam.text = "Yankees"
+    }
+    
+    
 
 }
 
